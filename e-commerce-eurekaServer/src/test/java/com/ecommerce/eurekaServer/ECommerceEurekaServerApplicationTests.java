ac082fbf66d0442dc1e1eb923bb36b066819e33b
Sreya1825 <gaddam.sreya@capgemini.com>
2025-03-28 14:03:45 +0530

    Add files via upload

---
+++ e-commerce-eurekaServer/src/test/java/com/ecommerce/eurekaServer/ECommerceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
